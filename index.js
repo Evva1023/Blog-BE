61651d67988f123da08d145f3ba1a7510f7d20d4
Evva1023 <evoosha@gmail.com>
2023-02-15 13:05:40 +0100

    Created routes folder with router files and added them in index.js file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
+import {authRouter} from "./routes/auth.js";
+import {postRouter} from "./routes/post.js";
+import {userRouter} from "./routes/user.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRouter);
+app.use("/post", postRouter);
+app.use("/user", userRouter);
 
-app.get("/", (req, res) => {
-    res.json("Works fine");
-});
 
 app.listen(8000, "localhost", () => console.log("Server running on http://localhost:8000"));
